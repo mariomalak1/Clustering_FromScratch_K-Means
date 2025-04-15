0b48c37861660177ca15eb2239a8f4d5adc801d4
mariomalak1 <marioalabd611@gmail.com>
2025-04-15 16:38:13 +0200

    class Load Data

--- clustering/loadData.py
+++ clustering/loadData.py
@@ -1,21 +1,25 @@
 import csv
 
-# validate the input "percentage_num_of_rows" -> percentage of the data needed to be read
-# if he put invalid number will be "50%"
-def validateNumOfRowsInput(percentage_num_of_rows):
-    try:
-        percentage_num_of_rows = float(percentage_num_of_rows)
-        if(percentage_num_of_rows < 0 or percentage_num_of_rows > 100):
-            percentage_num_of_rows = 50
-    except:
-        print("invalid number, will take default")
-        percentage_num_of_rows = 50
+class LoadData():
+    defaultPercentage = 50
+    
+    def __init__(self, file_path, percentage_num_of_rows):
+        self.file_path = file_path
 
-    return percentage_num_of_rows
+        # validate percentage_num_of_rows
+        try:
+            percentage_num_of_rows = float(percentage_num_of_rows)
+            if(percentage_num_of_rows < 0 or percentage_num_of_rows > 100):
+                percentage_num_of_rows = LoadData.defaultPercentage
+        except:
+            print("invalid number, will take default")
+            percentage_num_of_rows = LoadData.defaultPercentage
+
+        self.percentage_num_of_rows = percentage_num_of_rows
 
 
-# load the data from the file, and also put it in the data structure of "Point" 
-def loadDataFromFile(file_path, percentage_num_of_rows):
-    pass
+    # load the data from the file
+    def loadDataFromFile(self):
+        pass
 
 
