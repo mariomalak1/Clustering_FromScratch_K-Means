fc8b069309f103617df0d5aa9816be12ef27c87f
mariomalak1 <marioalabd611@gmail.com>
2025-04-15 18:09:01 +0200

    create normalization class - normalize data, convert categorical to numerical

---
+++ clustering/Normalization.py
@@ -0,0 +1,16 @@
+from .DataFrame import DataFrame 
+
+# take data as list of rows and normalize each field
+# can convert specific field to numerical if it's categorical
+class Normalization():
+    def __inti__(self, dataFrame: DataFrame):
+        self.dataFrame = dataFrame
+    
+    def convert_categorical_data_to_numerical(self, featureData):
+        pass
+
+    def min_max_normalization(self):
+        pass
+
+
+
